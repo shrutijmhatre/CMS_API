1b1c98206a098b0d4e1a0a1c07dda2a4df440f69
shrutijmhatre <shrutijmhatre@gmail.com>
2024-06-04 21:14:46 +0400

    Data model and migration

---
+++ CMS_API/Data/CustomerDbContext.cs
@@ -0,0 +1,15 @@
+﻿using CMS_API.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace CMS_API.Data
+{
+    public class CustomerDbContext: DbContext
+    {
+        public CustomerDbContext(DbContextOptions dbContextOptions): base(dbContextOptions)
+        {
+                
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
---
+++ CMS_API/Models/Domain/Customer.cs
@@ -0,0 +1,16 @@
+﻿namespace CMS_API.Models.Domain
+{
+    public class Customer
+    {
+        public Guid CustomerId { get; set; }
+        public required string FirstName { get; set; }
+
+        public required string LastName { get; set; }
+
+        public required string Email { get; set; }
+
+        public required string Phone { get; set; }
+
+        public string? Address { get; set; }
+    }
+}
